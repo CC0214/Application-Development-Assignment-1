85dbb4e26ea64cd9ad4b41179bcdaa047a0043ae
CC0214 <candyzhao214@gmail.com>
2022-04-24 00:05:35 -0400

    java file uploaded

---
+++ DiscountRate/Customer.java
@@ -0,0 +1,50 @@
+package DiscountRate;
+
+//--------------------------------------------------------------------
+//Assignment 1
+//Written by: Xin ZHAO 2152461
+//For Application Development 2 (Mobile) - Winter 2022
+//--------------------------------------------------------------------
+
+public class Customer {
+
+    private String name;
+    private boolean member;
+    private String memberType;
+
+    public Customer() {
+        this.member = false;
+    }
+
+    public Customer(String name, boolean member, String memberType) {
+        this.name = name;
+        this.member = member;
+        this.memberType = memberType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMember() {
+        return member;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String memberType) {
+        this.memberType = memberType;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", member=" + member +
+                ", memberType='" + memberType + '\'' +
+                '}';
+    }
+}
+
